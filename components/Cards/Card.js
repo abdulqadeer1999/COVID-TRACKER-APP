82f2312aabbfb91bdfe3b831b6494da5252a94e2
Maryam482 <jamilmaryam593@gmail.com>
2021-02-22 09:27:33 +0500

    covid

--- components/Cards/Card.js
+++ components/Cards/Card.js
@@ -7,7 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import './Cards.css'
 
-const useStyles = makeStyles((theme:Theme) =>
+const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
      // display: 'flex',
